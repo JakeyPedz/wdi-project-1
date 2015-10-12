23cd5b740793841f8d3278096f761af8f5d751f2
JakeyPedz <j.willisfleming@gmail.com>
2015-10-12 10:39:38 +0100

    Added Pseudo Text on Top of What Alex helped me with

--- js/app.js
+++ js/app.js
@@ -24,26 +24,34 @@
 // o Ratty from Wind in the Willows
 // o Peter Pettigrew/ Scabbers from Harry Potter
 // o Remy from Ratatouille 
-// o 
 // o Play sounds as well – so for example when someone gets hit it makes a sound.
 
-// Scoring system 
-
-// addEventListener 
-
+// When click on start iniate the being function for the game...
 $(function(){
   $('.start').on("click", begin);
 });
 
+// the score will being on 0 
 var score = 0;
 
+// The is the function which plays the game 
 function begin(){
+// When the game begins put the score back to 0. 
+  $('.score span').html(0);
+  score = 0;
+
+// This is referring to the li class
   var $lis = $('li');
+// The .length returns all of the elements 
   var numberOfLis = $lis.length;
 
+// Is this something to do with having 0 seconds between each one appearing (??)
+// i.e. as soon as one appears the other one can then appear (??)
   var timeLapsed   = 0;
+// The length of the game is 10 seconds (10,000 milliseconds)
   var lengthOfGame = 10000; 
-  
+
+// Not too sure about this one (???)
   var playing = setInterval(function(selected){
     $('.timer').html((lengthOfGame - timeLapsed) / 1000)
     timeLapsed+=1000;
@@ -51,7 +59,8 @@ function begin(){
     // Choose a random li using a random number as an index.
     // Math.floor will convert the random number to have no decimal places
     var selected = $lis[Math.floor(Math.random()*numberOfLis)];
-    
+
+// 
     $(selected).addClass("rat");
 
     var randomTime = Math.floor(Math.random()*1000) + 500;
@@ -72,7 +81,8 @@ function begin(){
   $('ul').on("click", ".rat", clickOnRat)
 } 
 
+// each time the rat is clicked add a score to the HTML 
 function clickOnRat(){
   score++;
-  $('.score').html(score);
+  $('.score span').html(score);
 }
