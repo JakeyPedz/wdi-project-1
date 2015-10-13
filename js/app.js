f2ec6c0ec5b8809f8446db8007edf156357fecb5
JakeyPedz <j.willisfleming@gmail.com>
2015-10-13 14:28:58 +0100

    Sound is working

--- js/app.js
+++ js/app.js
@@ -28,9 +28,15 @@
 
 
 // Next Problem to sort out
-// (i) Get the score to go back to 0 at the end of each game. 
-// (ii) Log the Scores of each player
-// (iii) Change the image of the rat when clicked
+// (i) Get the score to go back to 0 at the end of each game. - done 
+// (ii) Log the Scores of each player - done 
+// (iii) Change the image of the rat when clicked - done
+// (iv) Add a sword/mallet onMouse property to the image for both players 
+            // might have to add a class to the ul that says for the name of the person
+            // and then for every one 
+// (v) Add sound effects
+// (vi) Get gifs working - whoever wins, their Gif is not removed
+// (vii) Add player pics - done 
 
 
 $(function(){
@@ -44,24 +50,28 @@ var score        = 0;
 var turn         = 1
 var skinnerScore = 0;
 var mabelScore   = 0;
+// var audio        = new Audio('splat.mp3')
+
+
 
 
 
 function begin(){ 
   updateScoreBoard();
+  $('#skinnerLoses').hide()
+  $('#mabelLoses').hide()
+  $('#skinnerWins').hide()
+  $('#mabelWins').hide()
   $('#turn').html("")
   $('#slayAgain').html("")
   score = 0;
   $('.score span').html(score);
   
-
-
   var $lis = $('li');
-  console.log($lis)
+  // console.log($lis)
   var numberOfLis  = $lis.length;
   var timeLapsed   = 0;
   var lengthOfGame = 10000;
-
 
   var playing = setInterval(function(selected){
     $('.timer').html((lengthOfGame - timeLapsed) / 1000)
@@ -83,25 +93,37 @@ function begin(){
     console.log("Game has Ended");
 
     if(turn===1){
+      console.log("Skinner's Go")
       skinnerScore = score
       turn = 2
+      // $('.grid').css( 'cursor', 'url(skinnersword_360.png), auto' ); // doesnt work
+      // $('.grid').css( 'cursor', 'pointer' ); // doesnt work
+      $('.grid').addClass("skinnerCursor");
+      $('.grid').removeClass("mabelCursor");
       $('#turn').html("Mabels turn")
       updateScoreBoard();
 
     }else{
-
+      console.log("Mabel's Go")
       mabelScore = score
       turn = 1
+      // $(this).css( 'cursor', 'url(mabelmallet_360.png), auto' ); // doesnt work
+      $('.grid').addClass("mabelCursor");
+      $('.grid').removeClass("skinnerCursor");
 
       if(skinnerScore > mabelScore){
         // Skinner wins do this:
-        $('#turn').html("Skinner Wins with "+skinnerScore+" to "+mabelScore)
+        $('#turn').html("Skinner Wins with "+skinnerScore+" to "+mabelScore);
+        $('#mabelLoses').show(); // this is not working
+        $('#skinnerWins').show();
       }else if(skinnerScore===mabelScore){
         // If its a draw do this:
         $('#turn').html("Tie")
       }else{
         // Mabel wins do this:
-        $('#turn').html("Mabel Wins with "+mabelScore+" to "+skinnerScore)
+        $('#turn').html("Mabel Wins with "+mabelScore+" to "+skinnerScore);
+        $('#skinnerLoses').show(); 
+        $('#mabelWins').show();
       }
       updateScoreBoard();
       $('#slayAgain').html("Click start to slay again").css('color', 'red')
@@ -120,6 +142,7 @@ function clickOnRat(){
   score++;
   $('.score span').html(score);
   $self.addClass('dead-rat');
+  soundSplat();
 
   setTimeout(function(){
     $self.removeClass('dead-rat');
@@ -132,4 +155,19 @@ function updateScoreBoard(){
   $('.mabelScore').html(" "+mabelScore);
 
 }
+
+var soundSplat = function(){
+    var sound = soundManager.createSound({ 
+        id: 'soundSplat', 
+        url: './sounds/splat.mp3', 
+        volume: 50, 
+        autoPlay: true 
+    }).play();
+}
+
+// soundManager.setup({ 
+//  url: "/swf/",
+//  preferFlash: true,
+//  onready: begin  
+// })
 
